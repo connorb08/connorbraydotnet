98da7f8bbfebf1e77d631f7ad83196cf23767488
Connor Bray <connorb108@gmail.com>
2025-07-07 03:08:47 +0000

    container?

--- projects/linkedin-games/src/index.ts
+++ projects/linkedin-games/src/index.ts
@@ -1,6 +1,15 @@
 import PlayQueens from "./queens/index.ts";
 import PageController from "./queens/page-controller.ts";
+import http from "node:http";
 
-await using pageController = await PageController();
-const queenLocations = await PlayQueens({ pageController });
-console.log("Queen locations:", queenLocations);
+const server = http.createServer(async (req, res) => {
+	await using pageController = await PageController();
+	const queenLocations = await PlayQueens({ pageController });
+	res.statusCode = 200;
+	res.setHeader("Content-Type", "application/json");
+	res.end(JSON.stringify(queenLocations));
+});
+
+server.listen(3000, () => {
+	console.log("Server is running on http://localhost:3000");
+});
--- projects/linkedin-games/worker/index.ts
+++ projects/linkedin-games/worker/index.ts
@@ -1,6 +1,17 @@
+import { Container, getContainer } from "@cloudflare/containers";
+
+export class DockerContainer extends Container {
+	override sleepAfter = "10s";
+	override defaultPort = 3000;
+	override enableInternet = true;
+}
+
 export default {
 	async fetch(request, env, ctx): Promise<Response> {
 		try {
+			const container = getContainer(env.CONTAINER);
+			const res = await container.fetch(request);
+			console.log("Container response:", res);
 			return new Response("res");
 		} catch (error) {
 			return new Response("err");
