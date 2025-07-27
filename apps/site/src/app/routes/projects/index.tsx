f3a1c871755256ef4e833215b3a00f010936cf2e
Connor Bray <connorb108@gmail.com>
2025-07-27 17:34:17 +0000

    projects

--- apps/site/src/app/routes/projects/index.tsx
+++ apps/site/src/app/routes/projects/index.tsx
@@ -1,9 +1,5 @@
+import Projects from "#pages/projects";
+
 export default function () {
-	return (
-		<div>
-			<h1>Projects</h1>
-			<p>This is the projects page.</p>
-			<p>Here are some of my projects:</p>
-		</div>
-	);
+	return <Projects />;
 }
---
+++ apps/site/src/pages/projects/index.tsx
@@ -0,0 +1,11 @@
+import style from "./projects.module.scss";
+
+export default function Projects() {
+	return (
+		<div className={style.projects}>
+			<h1>Projects</h1>
+			<p>This is the projects page.</p>
+			<p>Here are some of my projects:</p>
+		</div>
+	);
+}
