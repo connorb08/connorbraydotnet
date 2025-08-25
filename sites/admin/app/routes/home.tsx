3dc69ce30f910d0fbc33084bc03427d62a15ada1
Connor Bray <connorb108@gmail.com>
2025-08-25 03:54:54 +0000

    remove queens

--- sites/admin/app/routes/home.tsx
+++ sites/admin/app/routes/home.tsx
@@ -1,4 +1,3 @@
-import { useSubmit } from "react-router";
 import type { Route } from "./+types/home";
 
 export function meta(_: Route.MetaArgs) {
@@ -12,38 +11,6 @@ export function loader({ context: _ }: Route.LoaderArgs) {
 	return {};
 }
 
-export async function action({ context }: Route.ActionArgs) {
-	try {
-		console.log("Action triggered, calling scheduled handler...");
-		const queens = context.cloudflare.env.QUEENS;
-		const res = await queens.scheduledHandler();
-		console.log(res);
-		return { result: res };
-	} catch (error) {
-		console.error("Error occurred while processing action:", error);
-		throw error;
-	}
-}
-
-export default function Home({ actionData: data }: Route.ComponentProps) {
-	const submit = useSubmit();
-
-	const handleSubmit = () => {
-		console.log("submitting...");
-		submit(
-			{},
-			{
-				method: "post",
-			},
-		);
-	};
-
-	return (
-		<div>
-			<button type="button" onClick={handleSubmit}>
-				Re-run queens
-			</button>
-			<p>{data ? JSON.stringify(data) : "Waiting..."}</p>
-		</div>
-	);
+export default function Home({}: Route.ComponentProps) {
+	return <div>hello</div>;
 }
