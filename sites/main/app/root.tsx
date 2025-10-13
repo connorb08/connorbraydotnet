fb0fb284c5b0ca4bf3841a1840fdce38ff815eca
Connor Bray <connorb108@gmail.com>
2025-10-13 01:14:17 +0000

    fonts

--- sites/main/app/root.tsx
+++ sites/main/app/root.tsx
@@ -1,19 +1,26 @@
 import { useRef } from "react";
-import {
-	isRouteErrorResponse,
-	Links,
-	Meta,
-	Outlet,
-	Scripts,
-	ScrollRestoration,
-	useLoaderData,
-} from "react-router";
+import { Links, Meta, Outlet, Scripts, ScrollRestoration, useLoaderData } from "react-router";
 import type { Route } from "./+types/root";
 import "#styles/App.scss";
 import { ContextProvider } from "../utils/state/context";
 import { getSession } from "./sessions.server";
 
-export const links: Route.LinksFunction = () => [];
+export const links: Route.LinksFunction = () => [
+	{
+		rel: "preload",
+		as: "font",
+		href: "/fonts/ibm-plex-mono-v20-latin-regular.woff2",
+		type: "font/woff2",
+		crossOrigin: "anonymous",
+	},
+	{
+		rel: "preload",
+		as: "font",
+		href: "/fonts/rajdhani-v17-latin-regular.woff2",
+		type: "font/woff2",
+		crossOrigin: "anonymous",
+	},
+];
 
 export const meta: Route.MetaFunction = () => {
 	return [{ title: "Connor Bray" }, { name: "description", content: "connorbray.net" }];
@@ -67,29 +74,6 @@ export default function App() {
 	return <Outlet />;
 }
 
-export function ErrorBoundary({ error }: Route.ErrorBoundaryProps) {
-	let message = "Oops!";
-	let details = "An unexpected error occurred.";
-	let stack: string | undefined;
-
-	if (isRouteErrorResponse(error)) {
-		message = error.status === 404 ? "404" : "Error";
-		details =
-			error.status === 404 ? "The requested page could not be found." : error.statusText || details;
-	} else if (import.meta.env.DEV && error && error instanceof Error) {
-		details = error.message;
-		stack = error.stack;
-	}
-
-	return (
-		<main className="r ptp16 container container">
-			<h1>{message}</h1>
-			<p>{details}</p>
-			{stack && (
-				<pre className="auto w-full w-full p-4 p-4">
-					<code>{stack}</code>
-				</pre>
-			)}
-		</main>
-	);
+export function ErrorBoundary({ error: _ }: Route.ErrorBoundaryProps) {
+	return <main>Error</main>;
 }
