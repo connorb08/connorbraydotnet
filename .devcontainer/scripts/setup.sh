c729637a6295cd4adc870ac27b4dd6db6c91541a
Connor Bray <connorb108@gmail.com>
2025-11-21 20:44:05 +0000

    setup

--- .devcontainer/scripts/setup.sh
+++ .devcontainer/scripts/setup.sh
@@ -4,3 +4,8 @@ SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 PARENT_DIR="$(dirname "$SCRIPT_DIR")"
 BASHRC_PATH="$PARENT_DIR/.bashrc"
 echo "source $BASHRC_PATH" >> ~/.bashrc
+
+export ROOT=$(pwd $(dirname "$(dirname "$(dirname "${BASH_SOURCE[0]}")")"))
+export DATABASE_DIR=$ROOT/services/database
+echo "DATABASE_DIR=$DATABASE_DIR" >> $GITHUB_ENV
+echo "ROOT=$ROOT" >> $GITHUB_ENV
