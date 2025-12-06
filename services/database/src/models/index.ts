32fd6419638a5d3310178e1a834403f5bcd27412
Connor Bray <connorb108@gmail.com>
2025-12-06 21:41:20 +0000

    deployment

--- services/database/src/models/index.ts
+++ services/database/src/models/index.ts
@@ -1,7 +1,10 @@
 import type { QueensTable } from "./queens";
+import type { RepositoryTable } from "./repository";
 
 export type Database = {
 	queens: QueensTable;
+	repository: RepositoryTable;
 };
 
 export type * from "./queens";
+export type * from "./repository";
--- services/database/src/models/queens.ts
+++ services/database/src/models/queens.ts
@@ -1,4 +1,4 @@
-import type { Insertable, JSONColumnType, Selectable, Updateable } from "kysely";
+import type { Insertable, JSONColumnType, SchemaModule, Selectable, Updateable } from "kysely";
 import type { QueensSolution } from "types";
 
 export type QueensTable = {
@@ -11,3 +11,12 @@ export type Queens = Omit<Selectable<QueensTable>, "id">;
 export type QueensWithId = Selectable<QueensTable>;
 export type NewQueens = Omit<Insertable<QueensTable>, "id" | "date">;
 export type QueensUpdate = Updateable<QueensTable>;
+
+export const createQueensTable = (schema: SchemaModule) =>
+	schema
+		.createTable("queens")
+		.ifNotExists()
+		.addColumn("id", "text", (col) => col.notNull().primaryKey())
+		.addColumn("date", "text", (col) => col.notNull())
+		.addColumn("solution", "text", (col) => col.notNull())
+		.execute();
