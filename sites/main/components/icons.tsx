2ff238b8fb44a82b122811adf5e424e10f9b7f54
Connor Bray <connorb108@gmail.com>
2026-03-01 17:40:28 +0000

    icons

--- sites/main/components/icons.tsx
+++ sites/main/components/icons.tsx
@@ -1,17 +1,17 @@
-export {
-	FaBars as Menu,
-	FaGithub as Github,
-	FaLinkedin as Linkedin,
-	FaRegFolder as Folder,
-} from "react-icons/fa";
-export { GoGear as Gear, GoMail as Mail, GoMoon as Moon, GoSun as Sun } from "react-icons/go";
-export { LuHouse as Home } from "react-icons/lu";
-export {
-	MdFullscreen as Fullscreen,
-	MdFullscreenExit as ExitFullscreen,
-	MdOutlinePhotoCamera as Camera,
-} from "react-icons/md";
+// export {
+// 	FaBars as Menu,
+// 	FaGithub as Github,
+// 	FaLinkedin as Linkedin,
+// 	FaRegFolder as Folder,
+// } from "react-icons/fa";
+// export { GoGear as Gear, GoMail as Mail, GoMoon as Moon, GoSun as Sun } from "react-icons/go";
+// export { LuHouse as Home } from "react-icons/lu";
+// export {
+// 	MdFullscreen as Fullscreen,
+// 	MdFullscreenExit as ExitFullscreen,
+// 	MdOutlinePhotoCamera as Camera,
+// } from "react-icons/md";
 
-export { RxChevronRight as Chevron } from "react-icons/rx";
-export { TbSitemap as Sitemap } from "react-icons/tb";
-export { VscAccount as About } from "react-icons/vsc";
+// export { RxChevronRight as Chevron } from "react-icons/rx";
+// export { TbSitemap as Sitemap } from "react-icons/tb";
+// export { VscAccount as About } from "react-icons/vsc";
