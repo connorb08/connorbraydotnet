ce775c65cf10f0c39c90c9327cc119a13d180bba
Connor Bray <connorb108@gmail.com>
2026-06-02 02:03:29 +0000

    update deps

--- sites/main/components/navigation/mobile.tsx
+++ sites/main/components/navigation/mobile.tsx
@@ -1,6 +1,12 @@
-import { Button, LinkButton } from "components/ui/buttons/button";
+import { Button, LinkButton } from "../ui/buttons/button";
 import { useState } from "react";
-import { Dialog, DialogTrigger, OverlayArrow, Popover, Separator } from "react-aria-components";
+import {
+	Dialog,
+	DialogTrigger,
+	OverlayArrow,
+	Popover,
+	Separator,
+} from "react-aria-components";
 import { useLocation } from "react-router";
 import { ToggleThemeButton } from "#components/toolbar";
 import { Menu as MenuIcon } from "../icons";
@@ -23,7 +29,11 @@ export default function MobileNavigation() {
 					size="medium"
 					onClick={() => setNavigationOpen(true)}
 				/>
-				<Popover className={style.Popover} isOpen={navigationOpen} onOpenChange={setNavigationOpen}>
+				<Popover
+					className={style.Popover}
+					isOpen={navigationOpen}
+					onOpenChange={setNavigationOpen}
+				>
 					<OverlayArrow className={style.Popover__Arrow} data-placement="top">
 						<svg width={12} height={12} viewBox="0 0 12 12" aria-hidden="true">
 							<path d="M0 0 L6 6 L12 0" />
