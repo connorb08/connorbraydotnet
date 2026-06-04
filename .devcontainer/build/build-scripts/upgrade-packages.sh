7a242c7f7a94ae3071d20a7c38eed72533cc16f7
Connor Bray <connorb108@gmail.com>
2026-06-04 22:23:43 +0000

    refactor devcontainer build

--- .devcontainer/build/build-scripts/upgrade-packages.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-
-set -e
-
-apt-get update -y
-apt-get install -y xauth --no-install-recommends
-apt-get -y upgrade --no-install-recommends
-apt-get autoremove -y
---
+++ .devcontainer/build/layers/bun.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+
+set -e
+
+curl -fsSL https://bun.com/install | bash
+
+bun -v
